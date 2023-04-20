a9a75b88156e799183a6fc3fa00d759849271a17
LemonkaPix <maciejwrobel029@gmail.com>
2023-04-20 14:56:53 +0200

    tablica

---
+++ dynamiczna_tablica_trojwymiarowa/dynamiczna_tablica_trojwymiarowa.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 3, y = 4, z = 5;
+    int*** tab = new int** [x];
+
+    for (int i = 0; i < x; ++i) {
+        tab[i] = new int* [y];
+        for (int j = 0; j < y; ++j) {
+            tab[i][j] = new int[z];
+            for (int k = 0; k < z; ++k) {
+                tab[i][j][k] = i * y * z + j * z + k;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < y; ++j) {
+            for (int k = 0; k < z; ++k) {
+                cout << tab[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < y; ++j) {
+            delete[] tab[i][j];
+        }
+        delete[] tab[i];
+    }
+    delete[] tab;
+
+}
